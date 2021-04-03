a8b48b2ca742b4686182d26ee81684055aeea003
wasp-soup <cheung.marcus@gmail.com>
2021-04-03 21:11:21 +0800

    test

--- main.py
+++ main.py
@@ -472,4 +472,4 @@ def get_token(session):
 
 # run server
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT')))
+    socketio.run(app, port=int(os.environ.get('PORT')))
