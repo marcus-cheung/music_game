ff51dee096de22d55533c5a0184ceb6f3af02cb7
wasp-soup <cheung.marcus@gmail.com>
2021-04-04 23:40:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def makeRoom(data):
         gamestates[room - 1000] = classes.GameState(
             song_infos = song_infos, host = session['unique'], gamemode = data['gamemode'], rounds = int(data['rounds']), users = [], room_number=room, password=data.get("password"), playlists = data['playlists']
         )
-        makeDir(room)
+        #makeDir(room)
         # Add songs to directory
         song_counter = 1
         for song in song_infos:
