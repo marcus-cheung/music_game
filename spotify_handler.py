6fec66d304279d5fdc3b94f01d4d2e9f4ce178f0
wasp-soup <cheung.marcus@gmail.com>
2021-04-14 22:45:36 +0800

    Update spotify_handler.py

--- spotify_handler.py
+++ spotify_handler.py
@@ -163,7 +163,8 @@ def validStatus(request):
 
 # Called on search
 def getArtistSearch(artist_name):
-    artist_info = getArtistInfos(artist_name, limit = 5)
+    limit = 5
+    artist_info = getArtistInfos(artist_name, limit = limit)
     print(artist_info)
-    payload = [{'image': artist_info[i]['images'][0]['url'], 'name': artist_info[i]['name'], 'id': artist_info[i]['id']} for i in range(5)]
+    payload = [{'image': artist_info[i]['images'][0]['url'], 'name': artist_info[i]['name'], 'id': artist_info[i]['id']} for i in range(limit)]
     return payload
