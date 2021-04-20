fcb3605312ed324862d794dc62abc4eaaa76d474
wasp-soup <cheung.marcus@gmail.com>
2021-04-20 21:59:00 +0800

    Update waveform.js

--- static/scripts/waveform.js
+++ static/scripts/waveform.js
@@ -1,4 +1,3 @@
-
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
     if (w < 2 * r) r = w / 2;
     if (h < 2 * r) r = h / 2;
@@ -23,8 +22,10 @@ function audio_visualizer(audioElement) {
     source.connect(analyser)
     source.connect(audioCtx.destination)
     let dataArray = new Uint8Array(analyser.frequencyBinCount)
+    console.log(dataArray)
 
     function loopingFunction() {
+        console.log(looping)
         requestAnimationFrame(loopingFunction)
         analyser.getByteFrequencyData(dataArray)
         draw(dataArray)
@@ -34,12 +35,14 @@ function audio_visualizer(audioElement) {
         let WIDTH = canvas.width
         let HEIGHT = canvas.height
         let CENTER = WIDTH / 2
+        console.log(WIDTH)
         canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
         let barWidth = (WIDTH * 1.0) / bufferLength / 2
+        console.log(barWidth)
         let barHeight;
         let x = 0;
         for (let i = 0; i < bufferLength; i++) {
-            barHeight = data[i]**1.5 / 300
+            barHeight = data[i] / 3
             canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
             canvasCtx.roundRect(CENTER + x, HEIGHT / 2 - barHeight / 2, barWidth, barHeight, barHeight / 10).fill();
             if (i != 0){
