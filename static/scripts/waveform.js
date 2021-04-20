cb314141524c9a12788d2234dcf0fffeb6ae9298
wasp-soup <cheung.marcus@gmail.com>
2021-04-20 21:38:20 +0800

    Update waveform.js

--- static/scripts/waveform.js
+++ static/scripts/waveform.js
@@ -1,54 +1,52 @@
 
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-                if (w < 2 * r) r = w / 2;
-                if (h < 2 * r) r = h / 2;
-                this.beginPath();
-                this.moveTo(x+r, y);
-                this.arcTo(x+w, y,   x+w, y+h, r);
-                this.arcTo(x+w, y+h, x,   y+h, r);
-                this.arcTo(x,   y+h, x,   y,   r);
-                this.arcTo(x,   y,   x+w, y,   r);
-                this.closePath();
-                return this;
-                }
+    if (w < 2 * r) r = w / 2;
+    if (h < 2 * r) r = h / 2;
+    this.beginPath();
+    this.moveTo(x+r, y);
+    this.arcTo(x+w, y,   x+w, y+h, r);
+    this.arcTo(x+w, y+h, x,   y+h, r);
+    this.arcTo(x,   y+h, x,   y,   r);
+    this.arcTo(x,   y,   x+w, y,   r);
+    this.closePath();
+    return this;
+}
                 
 function audio_visualizer(audioElement) {
-        let canvas = document.getElementById("audio_visualizer");
-        let canvasCtx = canvas.getContext("2d");
-        console.log(audioElement)
-        let audioCtx = new AudioContext();
-        let analyser = audioCtx.createAnalyser();
-        analyser.fftSize = 128
-        let bufferLength = analyser.frequencyBinCount
-        let source = audioCtx.createMediaElementSource(audioElement)
-        source.connect(analyser)
-        source.connect(audioCtx.destination)
-        let dataArray = new Uint8Array(analyser.frequencyBinCount)
+    let canvas = document.getElementById("audio_visualizer");
+    let canvasCtx = canvas.getContext("2d");
+    let audioCtx = new AudioContext();
+    let analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 128
+    let bufferLength = analyser.frequencyBinCount
+    let source = audioCtx.createMediaElementSource(audioElement)
+    source.connect(analyser)
+    source.connect(audioCtx.destination)
+    let dataArray = new Uint8Array(analyser.frequencyBinCount)
 
-        document.getElementById('start').onclick = () => {
-            audioElement.play()
-        }
-        function loopingFunction() {
-            requestAnimationFrame(loopingFunction)
-            analyser.getByteFrequencyData(dataArray)
-            draw(dataArray)
-        }
+    function loopingFunction() {
+        requestAnimationFrame(loopingFunction)
+        analyser.getByteFrequencyData(dataArray)
+        draw(dataArray)
+    }
 
-        function draw(data) {
-            let WIDTH = canvas.width
-            let HEIGHT = canvas.height
-            let CENTER = WIDTH/2
-            canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
-            let barWidth = (WIDTH * 1.0) / bufferLength / 2
-            let barHeight;
-            let x = 0;
-            for (let i = 0; i < bufferLength; i++) {
-                barHeight = data[bufferLength] / 2
-                canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
-                canvasCtx.roundRect(CENTER + x, HEIGHT/2 - barHeight/2, barWidth, barHeight, barHeight/10).fill();
-                canvasCtx.roundRect(CENTER - x - barHeight, HEIGHT/2 - barHeight/2, barWidth, barHeight, barHeight/10).fill();
-                x += barWidth + 1
+    function draw(data) {
+        let WIDTH = canvas.width
+        let HEIGHT = canvas.height
+        let CENTER = WIDTH / 2
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
+        let barWidth = (WIDTH * 1.0) / bufferLength / 2
+        let barHeight;
+        let x = 0;
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = data[i]**1.5 / 300
+            canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+            canvasCtx.roundRect(CENTER + x, HEIGHT / 2 - barHeight / 2, barWidth, barHeight, barHeight / 10).fill();
+            if (i != 0){
+                canvasCtx.roundRect(CENTER - x, HEIGHT / 2 - barHeight / 2, barWidth, barHeight, barHeight / 10).fill();
             }
+            x += barWidth + 1
         }
-        requestAnimationFrame(loopingFunction);
+    }
+    requestAnimationFrame(loopingFunction);
 }
